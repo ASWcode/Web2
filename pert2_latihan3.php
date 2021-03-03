4e66827374f0b54261b9d71fedc42325617d725e
ayu578 <68339357+ayu578@users.noreply.github.com>
2021-03-03 14:47:27 +0700

    pertemuan 2

---
+++ pert2_latihan3.php
@@ -0,0 +1,30 @@
+<?php
+?>
+<html>
+<head>
+<title>pertemuan 2 latihan 3 </title>
+</head>
+<body>
+<center>
+<table cellspacing="0" cellpadding="3">
+<tr>
+<td><font face="comic sans serif" size=5 color= "blue">Nilai I</font></td>
+<td></td>
+<td><font face="comic sans serif" size=5 color= "blue">Nilai II</font></td>
+</tr>
+<tr>
+<td><input type="text"></td>
+<td><select name="operator" id="operator">
+    <option value="-">-</option>
+    <option value="+">+</option>
+    <option value="/">/</option>
+    <option value="*">*</option>
+  </select></td>
+  <td><input type="text"></td>
+<td><input type="submit" value="Submit"></td>
+</tr> 
+
+</table>
+</center>
+</body>
+</html> 
